c2f9a791ffbd09484a7f1d413ad4df805f18120d
Amit Panthi <apapanthi@gmail.com>
2020-01-18 00:25:13 +0530

    Tweet retrieval works now, fixed the retrieval regex, and cleaned the code

--- api_query.py
+++ api_query.py
@@ -5,40 +5,47 @@ import csv
 import pandas as pd
 import re
 
-CONSUMER_KEY = "abc"
-CONSUMER_SECRET_KEY = "abc"
-ACCESS_TOKEN = "abc"
-ACCESS_TOKEN_SECRET = "abc"
+#auth declarations
+CONSUMER_KEY 		= "your-key-here"
+CONSUMER_SECRET_KEY = "your-skey-here"
+ACCESS_TOKEN 		= "your-token-here"
+ACCESS_TOKEN_SECRET = "your-stoken-here"
 
-auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
-auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
-
-api = tweepy.API(auth)
-
-csv_file = open('tweets.csv', 'a')
-csv_writer = csv.writer(csv_file)
-
-tweets = []
-
-REMOVE_STRAYS = r'\\x\w{2}'
+#regex definitions
+REMOVE_STRAYS 	= r'[^\x20-\x7e]' #creds to that guy on stackoverflow
 REMOVE_NEWLINES = r'\\n'
 
 
+tweets = []
+
+
 def clean_tweet(tweet):
-	stray_filter = re.sub(REMOVE_STRAYS, "", tweet)
-	newline_filter = re.sub(REMOVE_NEWLINES, " ", tweet)
+	tweet 			= tweet.decode('utf-8')
+	stray_filter 	= re.sub(REMOVE_STRAYS, "", tweet)
+	newline_filter 	= re.sub(REMOVE_NEWLINES, " ", stray_filter)
 
 	return newline_filter
 
 
-for tweet in tweepy.Cursor(api.search, q='#pinkfloyd -filter:retweets', count=2, tweet_mode='extended', lang='en').items():
-	unclean_tweet = str(tweet.full_text.encode('utf-8'))
-	cleaned_tweet = clean_tweet(unclean_tweet)
-	csv_writer.writerow([tweet.created_at, cleaned_tweet])
-	tweets.append(cleaned_tweet)
+def get_tweets():
+	for tweet in tweepy.Cursor(api.search, q='#pinkfloyd -filter:retweets', count=2, tweet_mode='extended', lang='en').items():
+		tweets.append(tweet.full_text.encode('utf-8'))
+
+	cleaned_tweets = []
+
+	for line in tweets:
+		cleaned_tweets.append(clean_tweet(line))
+
+	return cleaned_tweets
 
 
-df = pd.DataFrame(tweets)
-df.to_csv(r"df.csv")
+if __name__ == "__main__":
+	auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET_KEY)
+	auth.set_access_token(ACCESS_TOKEN, ACCESS_TOKEN_SECRET)
+	api = tweepy.API(auth)
 
-	
+	tweet_list = get_tweets()
+
+	df = pd.DataFrame(tweet_list)
+	df.to_csv(r"df.csv")
+
